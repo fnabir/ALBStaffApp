00ef0f1c480aa0e7ad8ff39ead22d0b808f54a39
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-07-30 19:52:01 +0100

    Remove redundant model files

--- app/src/main/java/com/asianliftbd/staff/user/UApp.java
+++
@@ -1,8 +0,0 @@
-package com.asianliftbd.staff.user;
-public class UApp {private String name,date,feature,link;private int code;public UApp(){}
-    public String getLink(){return link;}
-    public String getName(){return name;}
-    public String getDate(){return date;}
-    public String getFeature(){return feature;}
-    public int getCode(){return code;}
-}
--- app/src/main/java/com/asianliftbd/staff/user/UInfo.java
+++
@@ -1,6 +0,0 @@
-package com.asianliftbd.staff.user;
-    public class UInfo {private String title;private int roll;
-    public UInfo(){}
-        public String getTitle(){return title;}
-        public int getRoll(){return roll;}
-}
